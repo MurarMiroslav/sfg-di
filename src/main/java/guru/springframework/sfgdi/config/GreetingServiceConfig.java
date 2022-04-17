660970f59d01ec6b984f2802cda98f12a8b26bd9
Miroslav <mmurar@scoop-software.cz>
2022-04-17 14:02:41 +0200

    Very important how to transform another SERVICE with PROFILE to the BEANs

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,13 +1,38 @@
 package guru.springframework.sfgdi.config;
 
 import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.I18nEnglishGreetingService;
+import guru.springframework.sfgdi.services.I18nSpanishGreetingService;
+import guru.springframework.sfgdi.services.PrimaryGreetingService;
 import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
 import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+	@Profile({"ES", "default"})
+	@Bean("i18nService") //Pretoze Java ti neuzmoni mat 2x rovnaky nazov metody s rovnakymi parametrami tak sa to obchadza takto ze metoda ma
+					     //sice iny nazov ale BEAN sa vola rovnako pomocou deklaracie v @Bean, o konkretnej implementacii sa rozhodne v RUNTIME
+	   					 //bna zaklade @Profile
+	I18nSpanishGreetingService i18NSpanishService() {
+		return new I18nSpanishGreetingService();
+	}
+
+	@Profile("EN")
+	@Bean
+	I18nEnglishGreetingService i18nService() {
+		return new I18nEnglishGreetingService();
+	}
+
+	@Primary
+	@Bean
+	PrimaryGreetingService primaryGreetingService() {
+		return new PrimaryGreetingService();
+	}
 
 	@Bean
 	ConstructorGreetingService constructorGreetingService() {
